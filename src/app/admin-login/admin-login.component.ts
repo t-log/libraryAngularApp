c2c3562f698beeef7dbba4458513589524bf5e1e
akhil <akhil.paul@ymail.com>
2022-11-30 10:52:45 +0530

    Adds navigation in admin login

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
@@ -9,15 +10,20 @@ export class AdminLoginComponent {
   username = ""
   password = ""
 
+  constructor(private router:Router){}
+
   adminEntry = () =>
   {
     let adminData:any = {"username":this.username,"password":this.password}
     console.log(adminData)
     if (this.username =="admin" && this.password =="12345") {
       alert("Valid Login")
+      this.router.navigate(['/entry'])
     }else
     {
       alert("Invalid Login")
+      this.username=""
+      this.password=""
     }
   }
 
